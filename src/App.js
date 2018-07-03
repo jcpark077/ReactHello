9839e0c9d29bb4b865f0f54cd51c09a56c5e5762
PJC <jcpark077@gmail.com>
2018-07-03 15:56:12 +0900

    강의목표 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const goals = ["1.xxxx", "2.yyyy", "3.zzz", "4.xxxxxxxxxxxx", "5.gkskddddddddddddddddd"];
+
+const LectGList = props => {
+  return (
+    <div>
+      <div>{props.title}</div>
+      <ul>{props.items.map((item, idx) => <li key={idx}>{item}</li>)}</ul>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
@@ -13,6 +24,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <LectGList items={goals} title={"강의   목표!!!!=======!"} />
       </div>
     );
   }
